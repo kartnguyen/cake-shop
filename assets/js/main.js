eef85ca02f63afc98d91b30f39b641e282540318
Thanh Nguyen <127193044+kartnguyen@users.noreply.github.com>
2023-07-10 07:34:18 +0700

    commit shop

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+const api_url = "https://64a662ab096b3f0fcc7fa7e0.mockapi.io/";
+
+function check_width () {
+  if (screen.width < 1023) {
+    document.querySelector("nav").className = '';
+    console.log('asds')
+  }
+}
+check_width();
+
+window.addEventListener("scroll", function (e) {
+  if (window.pageYOffset > 20) {
+    document.querySelector("header").classList.add("fixed-top");
+    document
+      .querySelector(".back-to-top")
+      .classList.add("show", "animated", "infinite", "pulse");
+  } else {
+    document.querySelector("header").classList.remove("fixed-top");
+    document
+      .querySelector(".back-to-top")
+      .classList.remove("show", "animated", "infinite", "pulse");
+  }
+});
+
+let template = document.body;
+
+function handle_mobile_nav() {
+  let btn = template.querySelector(".navbar-dropdown"),
+    nav = template.querySelector("header nav"),
+    overlay = document.createElement("div");
+  overlay.classList.add("overlay");
+
+  if (btn) {
+    btn.addEventListener("click", (e) => {
+      template.classList.add("overflow-hidden");
+      template.querySelector("header .row").appendChild(overlay);
+      nav.className = "show animated bounceInLeft";
+    });
+
+    overlay.addEventListener("click", (e) => {
+      template.classList.remove("overflow-hidden");
+      overlay.remove();
+      nav.className = "show animated bounceOutLeft";
+      setTimeout(() => {
+        nav.className = "";
+      }, 300);
+    });
+  }
+}
+handle_mobile_nav();
+
+$(".owl-carousel").owlCarousel({
+  items: 1,
+  nav: false,
+  dots: false,
+  animateOut: "fadeOut",
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayTimeout: 2000,
+});
---
+++ render_client.js
@@ -0,0 +1,158 @@
+module.exports = {
+  html() {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>KarT's House - The Best Bakery Shop</title>
+        <!-- favicon -->
+        <link rel="shortcut icon" href="./assets/images/icon.png" type="image/x-icon">
+        <!-- fontawesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+            integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- google font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
+        <!-- aos -->
+        <link rel="stylesheet" href="./assets/libs/aos-master/aos.css">
+        <!-- animate -->
+        <link rel="stylesheet" href="./assets/libs/animate/animate.css">
+        <!-- owlcarousel -->
+        <link rel="stylesheet" href="./assets/libs/owlcarousel/assets/owl.carousel.min.css">
+        <link rel="stylesheet" href="./assets/libs/owlcarousel/assets/owl.theme.default.min.css">
+        <!-- main css -->
+        <link rel="stylesheet" href="./assets/css/style.css">
+        <!-- home css -->
+        <link rel="stylesheet" href="./assets/css/home.css">
+    </head>
+    
+    <body>
+        <header>
+            <div class="container" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500">
+                <div class="row">
+                    <a href="index.html" title="KarT's House">
+                        <img src="./assets/images/logo.svg" alt="">
+                    </a>
+                    <div class="navbar-toggle">
+                        <div class="login">
+                            <a href="">
+                                <button title="Giỏ hàng">
+                                    <i class="fa-solid fa-cart-shopping bounceIn"></i>
+                                </button>
+                            </a>
+                            <button>Login</button>
+                        </div>
+                        <button class="navbar-dropdown">
+                            <i class="fa-solid fa-bars"></i>
+                        </button>
+                    </div>
+                    <nav>
+                        <div class="nav-logo">
+                            <img src="./assets/images/logo.svg" alt="">
+                        </div>
+                        <div class="nav-bar">
+                            <a href="/" class="home active">Trang chủ</a>
+                            <a href="/products" class="products">Sản phẩm</a>
+                            <a href="/about" class="about">Về chúng tôi</a>
+                            <a href="/contact" class="contact">Liên hệ</a>
+                        </div>
+                        <div class="login">
+                            <a href="/cart">
+                                <button title="Giỏ hàng">
+                                    <i class="fa-solid fa-cart-shopping"></i>
+                                </button>
+                            </a>
+                            <button>Login</button>
+                        </div>
+                        <div class="nav-img" style="background-image: url(./assets/images/nav-bar.webp);"></div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <main>
+            
+        </main>
+        <footer>
+            <div class="container footer">
+                <div class="row">
+                    <div class="img">
+                        <img src="./assets/images/logo.svg" alt="">
+                    </div>
+                    <div class="col">
+                        <div class="name">
+                            <h3>Thời gian làm việc</h3>
+                        </div>
+                        <div class="content">
+                            <p>Thứ 2 - Thứ 6: 8 am - 8 pm</p>
+                            <p>Thứ 7: 9am - 4pm</p>
+                            <p>Chủ nhật: Đóng cửa</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="name">
+                            <h3>Địa chỉ</h3>
+                        </div>
+                        <div class="content">
+                            <p>Kart's House</p>
+                            <p>Số 94 Yên Nghĩa, Hà Đông, Hà Nội</p>
+                            <p>thanhnguyen.gt09@gmail.com</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="name">
+                            <h3>Mạng xã hội</h3>
+                        </div>
+                        <div class="content">
+                            <div>
+                                <a href="#">
+                                    <i class="fa-brands fa-facebook fa-2x" style="color: #453cedc6;"></i>
+                                    Facebook
+                                </a>
+                                <a href="#">
+                                    <i class=" fa-2x fa-brands fa-instagram" style="color: #ff6d2e;"></i>
+                                    Instagram
+                                </a>
+                            </div>
+                            <div class="social">
+                                <a href="#">
+                                    <i class="fa-2x fa-brands fa-youtube" style="color: #d70f0f;"></i>
+                                    Youtube
+                                </a>
+                                <a href="#" style="margin-left: 18px;" class="twitter">
+                                    <i class="fa-2x fa-brands fa-twitter" style="color: #57e3ff;"></i>
+                                    Twitter
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    
+            </div>
+            <div class="container">
+                <div class="copyright">
+                    <span>Copyright © 2023-Powered by Thanh Nguyen</span>
+                </div>
+            </div>
+        </footer>
+        <a class="back-to-top" href="#" title="Lên đầu trang">
+            <i class="fa-solid fa-chevron-up" style="color: #ffffff;"></i>
+        </a>
+        <script src="./assets/js/components/jquery-3.7.0.min.js"></script>
+        <script src="./assets/libs/owlcarousel/owl.carousel.min.js"></script>
+        <script src="./assets/libs/aos-master/aos.js"></script>
+        <script src="./assets/js/main.js"></script>
+        <script>
+            AOS.init();
+        </script>
+    </body>
+    
+    </html>
+    `;
+  }
+}
