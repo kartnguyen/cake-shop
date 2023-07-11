09ac5d70d9640db2c8cd9c57d4440edca689824c
Thanh Nguyen <thanhnguyen.gt09@gmail.com>
2023-07-11 17:24:55 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const client_view = require('./render_client.js');
+const port = 3000;
 
-app.use('/assets', express.static('assets'));
-app.listen(3000, () => console.log('Services start on port 3000'));
+app.use(express.static(__dirname + '/public'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.get('/*', (req, res, next) => {
   res.setHeader('Content-Type', 'text/html');
