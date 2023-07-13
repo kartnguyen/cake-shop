fcf85c316089d820b571d74888982421b2f59b96
Thanh Nguyen <thanhnguyen.gt09@gmail.com>
2023-07-13 17:49:25 +0700

    Update render.js

--- assets/js/render.js
+++ assets/js/render.js
@@ -4,33 +4,36 @@ import { removeLoader } from "./components/help.js";
 let app = document.querySelector("main");
 
 async function init_app() {
-  app.appendChild(loading.cake_gif());
-
   if (location.pathname == "/") {
+    app.appendChild(loading.cake_gif());
     setTimeout(removeLoader, 3600);
     let page = await import("./pages/home.js");
     let render = await page.render();
     app.appendChild(await render);
   }
   if (location.pathname.includes("products")) {
+    app.appendChild(loading.cake_gif());
     setTimeout(removeLoader, 1500);
     let page = await import("./pages/products.js");
     let render = await page.render();
     app.appendChild(await render);
   }
   if (location.pathname.includes("about")) {
+    app.appendChild(loading.cake_gif());
     setTimeout(removeLoader, 1500);
     let page = await import("./pages/about.js");
     let render = await page.render();
     app.appendChild(await render);
   }
   if (location.pathname.includes("contact")) {
+    app.appendChild(loading.cake_gif());
     setTimeout(removeLoader, 1500);
     let page = await import("./pages/contact.js");
     let render = await page.render();
     app.appendChild(await render);
   }
   if (location.pathname.includes("cart")) {
+    app.appendChild(loading.cake_gif());
     setTimeout(removeLoader, 1500);
     let page = await import("./pages/cart.js");
     let render = await page.render();
